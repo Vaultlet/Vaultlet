8d72ab3c25664c516a62976978c9bf4b0e68a88e
Steven Li <steven.li@gsa.gov>
2025-04-25 16:59:17 -0400

    update setup.py

--- vaultlet/setup.py
+++ vaultlet/setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from setuptools.command.install import install
 import subprocess
 import sys
+import os
 
 class PostInstallCommand(install):
     """Post-installation for installation mode."""
@@ -10,6 +11,22 @@ class PostInstallCommand(install):
         subprocess.call([sys.executable, 'post_install.py'])
 
 setup(
+    name='vaultlet',
+    version='1.0.0',
+    description='Securely inject env vars using Windows Credential Manager',
+    author='Steven Li',
+    author_email='stevenli6186@gmail.com',
+    license='MIT',
+    packages=find_packages(include=['vaultlet', 'vaultlet.*']),
+    install_requires=[
+        'keyring',
+    ],
+    entry_points={
+        'console_scripts': [
+            'vaultlet=vaultlet.__main__:main',
+            'vl=vaultlet.__main__:main',
+        ],
+    },
     cmdclass={
         'install': PostInstallCommand,
     },
