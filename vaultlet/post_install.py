de0a6be9d0f0276fce71aa3008035800523c1b9c
Steven Li <steven.li@gsa.gov>
2025-04-25 16:41:57 -0400

    fix: add post install and setup py files

---
+++ vaultlet/post_install.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import subprocess
+
+def add_to_path():
+    path_to_add = os.path.join(sys.prefix, 'Scripts')
+    if os.name == 'nt':
+        # Windows
+        subprocess.call(['setx', 'PATH', f'%PATH%;{path_to_add}'])
+    else:
+        # Unix-based systems
+        bashrc_path = os.path.expanduser('~/.bashrc')
+        with open(bashrc_path, 'a') as bashrc:
+            bashrc.write(f'\nexport PATH=$PATH:{path_to_add}\n')
+        subprocess.call(['source', bashrc_path])
+
+if __name__ == '__main__':
+    add_to_path()
+    
